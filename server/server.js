3520c2013a9919a4f7804dbf3bd1cf092adbdc71
Mohan Kumar <sweetperson.mk@gmail.com>
2017-07-08 13:49:36 +0530

    Get todos route completed

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post("/todos", (req, res)=>{
 
 });
 
+app.get("/todos", (req, res) => {
+
+    Todo.find().then((todo)=>{
+        res.send({todo});
+    }, (err) => {
+       res.status(400).send(err);
+    });
+});
+
 
 app.listen(3000, () => {
    console.log("server is on port 3000");
